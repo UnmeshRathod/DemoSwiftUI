4cc446b6d54fc3aca575871fbec2c3dac9d00318
Unmesh <unmesh@sensibol.com>
2019-06-15 15:24:36 +0530

    Initial Commit

---
+++ DemoSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DemoSwiftUI
+//
+//  Created by Unmesh Rathod on 15/06/19.
+//  Copyright © 2019 UnmeshRathod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
